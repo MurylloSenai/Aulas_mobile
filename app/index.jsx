372ef5d2178fd6ded70018951715cc6f1602a064
MurylloSenai <muryllo.teixeira@aluno.senai.br>
2025-03-06 09:00:15 -0300

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,44 @@
+import { Button, Text, View, StyleSheet, TextInput } from "react-native";
+
+// export default function App(){
+//     return(
+//         <View>
+//             <Text style={estilo.texto}>  
+//                 Hello World
+//             </Text>
+//             <Button 
+//                 title="Clique em mim"
+//             />
+//         </View>
+//     );
+// }
+
+export default function login(){
+    return(
+        <View>
+            <View>
+                <Text>Entrar</Text>
+            </View>
+            <View>
+                <TextInput
+                    placeholder="email"
+                />
+                <TextInput
+                    placeholder="senha"
+                />
+            </View>
+            <View>
+             <Button title="entrar" />
+             <Text>cadastre-se</Text>
+            </View>
+        </View>
+    );
+}
+
+const estilo = StyleSheet.create({
+    texto: {
+        color:'#04bac3'
+    }
+
+});
+
