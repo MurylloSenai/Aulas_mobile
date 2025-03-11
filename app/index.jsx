033b13a7a82279b852dc14e9e8365a99b681ac4e
MurylloSenai <muryllo.teixeira@aluno.senai.br>
2025-03-11 09:01:32 -0300

    Mensagem do commit

--- app/index.jsx
+++ app/index.jsx
@@ -1,3 +1,8 @@
+// npm run start --tunel
+//npx expo install --check
+// npm run start --clear
+
+
 
 import { View, Text, TextInput, Pressable, StyleSheet } from "react-native";
 
@@ -10,7 +15,7 @@ export default function Login() {
         <Text style={styles.title}>Login</Text>
         <Image
           source={require('./assets/images/user-profile-icon-free-vector.jpg')} // Atualize o caminho conforme necessário
-          style={styles.profileIcon}
+          style={styles.Icon}
         />
       </View>
 
@@ -118,3 +123,89 @@ const styles = StyleSheet.create({
     textAlign: 'center'
   },
 });
+
+
+// const container = styled.View `
+//   flex: 1,
+//   backgroundColor: "#1B263B",
+//   padding: 26,
+// `
+
+
+// const header = styled.View`
+//    rginTop: 80,
+    // marginBottom: 110,
+// `
+
+// const title = styled.View `
+//    fontSize: 60,
+    // color: "#EDF2F4",
+    // fontWeight: 'semibold',
+// `
+
+// const Icon = styled.View`
+//    width: 60, // Ajuste a largura da imagem
+    // height: 60, // Ajuste a altura da imagem
+    // marginTop: 10,
+// ` 
+
+
+// const campos = styled.View`
+//    gap: 15,
+    // alignItems: 'center',
+// `
+
+// const campoTexto = styled.View`
+//    width: 344,
+    // height: 60,
+    // backgroundColor: "#EDF2F4",
+    // borderRadius: 20,
+    // textAlignVertical: 'center',
+    // paddingHorizontal: 16,
+    // fontSize: 16,
+    // color: "#000",
+    // borderColor: "#88CFEC",
+    // borderWidth: 3,
+// ` 
+
+
+// const esqueci_senha = styled.View`
+//   color: '#88CFEC',
+    // alignSelf: 'flex-end',
+    // marginRight: 16,
+    // marginBottom: 20,
+    // fontSize: 14,
+// ` 
+
+
+// const acoes = styled.View `
+//  gap: 20,
+    // marginTop: 10,
+// `
+
+// const button = styled.View `
+//    width: 294, 
+    // height: 50,
+    // alignItems: 'center',
+    // justifyContent: 'center', 
+    // backgroundColor: '#88CFEC',
+    // borderRadius: 20,
+    // padding: 10,
+    // alignSelf: 'center', 
+// `
+
+
+// const buttonText = styled.View´
+//   color: "#000",
+    // fontSize: 25,
+    // fontWeight: 'regular',
+// ´ 
+
+
+// const cadastrar = styled.View´
+//    color: "#88CFEC",
+//  fontSize: 14,
+//  textAlign: 'center'
+// ´
+// 
+// 
